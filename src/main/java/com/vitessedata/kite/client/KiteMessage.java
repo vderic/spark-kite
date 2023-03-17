11b4223de92b83e843ef08a38ba39eda5240cf43
eric <ericlam@vitessedata.com>
2023-03-17 12:14:14 +0000

    kite client

---
+++ src/main/java/com/vitessedata/kite/client/KiteMessage.java
@@ -0,0 +1,50 @@
+package com.vitessedata.kite.client;
+
+import java.lang.System;
+
+public class KiteMessage {
+
+    public static final byte[] KIT1 = { 'K', 'I', 'T', '1' };
+    public static final byte[] JSON = { 'J', 'S', 'O', 'N' };
+    public static final byte[] BYE = { 'B', 'Y', 'E', '_' };
+    public static final byte[] ERROR = { 'E', 'R', 'R', '_' };
+    public static final byte[] VECTOR = { 'V', 'E', 'C', '_' };
+
+    private byte[] buffer = null;
+    private int msglen = 0;
+    private byte[] msgty = null;
+
+    public KiteMessage() {
+        msgty = new byte[4];
+        buffer = new byte[1024];
+        msglen = 0;
+
+    }
+
+    public byte[] getMessageType() {
+        return msgty;
+    }
+
+    public int getMessageLength() {
+        return msglen;
+    }
+
+    public byte[] getMessage() {
+        return buffer;
+    }
+
+    /* will set the message length and realloc buffer */
+    public byte[] setMessage(byte[] msgty, int bufsz) {
+        System.arraycopy(msgty, 0, this.msgty, 0, this.msgty.length);
+        msglen = bufsz;
+
+        // realloc buffer
+        if (buffer.length < bufsz) {
+            buffer = new byte[bufsz];
+            return buffer;
+        }
+
+        return buffer;
+    }
+
+}
---
+++ src/main/java/com/vitessedata/kite/client/SockStream.java
@@ -0,0 +1,74 @@
+package com.vitessedata.kite.client;
+
+import java.net.*;
+import java.io.*;
+import java.nio.ByteBuffer;
+
+public class SockStream {
+
+    private Socket socket;
+    private InputStream in;
+    private OutputStream out;
+
+    public SockStream(Socket socket) throws IOException {
+        this.socket = socket;
+        in = socket.getInputStream();
+        out = socket.getOutputStream();
+    }
+
+    public void close() throws IOException {
+        socket.close();
+    }
+
+    private void readfully(byte[] msg, int msgsz) throws IOException {
+        int p = 0;
+        int len = msgsz;
+
+        while (p < msgsz) {
+            int n = in.read(msg, p, len);
+            if (n <= 0) {
+                throw new IOException("socket unexpected EOF");
+            }
+            if (n > 0) {
+                p += n;
+                len -= n;
+                continue;
+            }
+        }
+    }
+
+    public void send(byte[] msgty, byte[] msg) throws IOException {
+
+        if (msg == null) {
+            throw new IOException("SockStream send(): msg cannot be NULL");
+        }
+
+        int msgsz = msg.length;
+        String hex = String.format("%08X", msgsz);
+        ByteBuffer meta = ByteBuffer.allocate(12);
+        meta.put(msgty);
+        meta.put(hex.getBytes());
+
+        out.write(meta.array());
+        out.write(msg);
+    }
+
+    public void recv(KiteMessage msg) throws IOException {
+
+        ByteBuffer meta = ByteBuffer.allocate(12);
+        readfully(meta.array(), meta.array().length);
+
+        byte[] msgty = new byte[4];
+        byte[] hex = new byte[8];
+
+        meta.get(msgty);
+        meta.get(hex);
+
+        int msglen = Integer.parseInt(new String(hex), 16);
+
+        byte[] buf = msg.setMessage(msgty, msglen);
+        readfully(buf, msglen);
+
+    }
+
+}
