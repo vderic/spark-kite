8c76ae78052bacb960220c5145380d3e3a46ab63
eric <ericlam@vitessedata.com>
2023-03-17 18:16:10 +0000

    request update

--- src/main/java/com/vitessedata/kite/sdk/Request.java
+++ src/main/java/com/vitessedata/kite/sdk/Request.java
@@ -11,10 +11,6 @@ public class Request {
     private FileSpec filespec;
     private JSONObject json;
 
-    public Request() {
-        json = new JSONObject();
-    }
-
     public Request(String schema, String sql, int fragid, int fragcnt, FileSpec filespec) {
         this.schema = schema;
         this.sql = sql;
@@ -24,7 +20,7 @@ public class Request {
 
         json = new JSONObject();
 
-        json.put("schema", schema);
+        json.put("schema", schema2JSON(schema));
         json.put("sql", sql);
         json.put("fragment", new JSONArray().put(fragid).put(fragcnt));
 
@@ -39,13 +35,29 @@ public class Request {
 
     }
 
+    private JSONArray schema2JSON(String schema) {
+
+        JSONArray array = new JSONArray();
+        String[] lines = schema.split("\n");
+
+        for (int i = 0; i < lines.length; i++) {
+            String[] column = lines[i].split(":", 4);
+            array.put(new JSONObject().put("name", column[0]).put("type", column[1])
+                    .put("precision", Integer.parseInt(column[2])).put("scale", Integer.parseInt(column[3])));
+        }
+
+        return array;
+    }
+
     public String toString() {
         return json.toString();
     }
 
     public static void main(String[] args) {
 
-        Request req = new Request("schema", "sql", 0, 1, new CsvFileSpec(',', '"', '"', false, "NULL"));
+        String schema = "orderid:int64:0:0\ncost:fp64:0:0\ntotal:decimal:28:3";
+        String sql = "select * from lineitem*";
+        Request req = new Request(schema, sql, 0, 1, new CsvFileSpec(',', '"', '"', false, "NULL"));
         System.out.println(req.toString());
     }
 
