56a1539169136aba77cb0775daf78324f33461e7
eric <ericlam@vitessedata.com>
2023-03-22 09:02:17 +0000

    tablename as input parameter

--- src/main/java/com/vitessedata/test/CsvDataSourceRunner.java
+++ src/main/java/com/vitessedata/test/CsvDataSourceRunner.java
@@ -25,17 +25,16 @@ import java.io.FileInputStream;
 
 public class CsvDataSourceRunner {
 
-    private static final String TABLE_NAME = "lineitem";
-
     public static void main(String[] args) {
 
-        if (args.length != 2) {
-            System.out.println("java class schemafn sqlfn");
+        if (args.length != 3) {
+            System.out.println("java class tablename schemafn sqlfn");
             return;
         }
 
-        String schemafn = args[0];
-        String sqlfn = args[1];
+        String tablename = args[0];
+        String schemafn = args[1];
+        String sqlfn = args[2];
 
         StructType schema = null;
         String sql = null;
@@ -65,7 +64,7 @@ public class CsvDataSourceRunner {
         Dataset<Row> dataset = sparkSession.read().schema(schema).format("kite").option("host", "localhost:7878")
                 .option("path", "test_tpch/csv/lineitem*").option("filespec", "csv").option("fragcnt", 2).load();
 
-        dataset.createOrReplaceTempView(TABLE_NAME);
+        dataset.createOrReplaceTempView(tablename);
         sparkSession.sql(sql).show(false);
 
         /* temp view */
